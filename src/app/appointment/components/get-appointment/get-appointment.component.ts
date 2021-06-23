a6f477d7bee2f2d5667194b48d117a6f4fa8fa08
Sebastián Zuluaga Gómez - Ceiba Software <sebastian.zuluaga@ceiba.com.co>
2021-06-23 17:31:27 -0500

    visual appointment status

--- src/app/appointment/components/get-appointment/get-appointment.component.ts
+++ src/app/appointment/components/get-appointment/get-appointment.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AppointmentServiceService } from '../../shared/service/appointment-service.service';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-get-appointment',
@@ -13,23 +15,51 @@ export class GetAppointmentComponent implements OnInit {
 
   public appointment$!: Observable<any[]>;
   constructor(
-    private appointmentService: AppointmentServiceService
+    private appointmentService: AppointmentServiceService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
     this.appointment$ = this.appointmentService.getAppointment();
   }
 
+  redirectTo(uri:string){
+    this.router.navigateByUrl('/', {skipLocationChange: true}).then(()=>
+    this.router.navigate([uri]));
+ }
+
   public accept(id: String){
     this.idCitaPorFila = id;
     console.log(this.idCitaPorFila)
-    this.appointmentService.acceptAppointment(this.idCitaPorFila).subscribe();
+    this.appointmentService.acceptAppointment(this.idCitaPorFila).subscribe(resp => {
+      Swal.fire({
+        title: '<p class="fuente size-fuente" style="color: #80d8ff"><small>¡Genial! Cita aceptada.</small></p>',
+        html: '<p class="fuente size-fuente" style="color: #ffffff"><small></small></p>',
+        icon: 'success',
+        confirmButtonColor: '#00e17b',
+        background: '#212121',
+        confirmButtonText: '<a class="fuente">Ok</a>'
+      });
+      //this.router.navigateByUrl('appointment/get');
+      this.redirectTo('/appointment/get');
+    });
   }
 
   public reject(id: String){
     this.idCitaPorFila = id;
     console.log(this.idCitaPorFila)
-    this.appointmentService.rejectAppointment(this.idCitaPorFila).subscribe();
+    this.appointmentService.rejectAppointment(this.idCitaPorFila).subscribe(resp=>{
+      Swal.fire({
+        title: '<p class="fuente size-fuente" style="color: #80d8ff"><small>Vaya! Cita rechazada.</small></p>',
+        html: '<p class="fuente size-fuente" style="color: #ffffff"><small>en cualquier momento la puede aceptar</small></p>',
+        icon: 'warning',
+        confirmButtonColor: '#00e17b',
+        background: '#212121',
+        confirmButtonText: '<a class="fuente">Ok</a>'
+      });
+      //this.router.navigateByUrl('appointment/get');
+      this.redirectTo('/appointment/get');
+    });
   }
 
 
